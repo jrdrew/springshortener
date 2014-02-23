94858b2d6233b7fd9b7fcb70659b8c194560e098
Jonathan Drew <jd@fanatic.co>
2014-02-23 07:01:47 -0500

    initial hello world test to see if things are wired correctly in my IDE

---
+++ src/test/java/org/jrdrew/shortener/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.jrdrew.shortener;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jonathandrew
+ * Date: 2/23/14
+ * Time: 6:59 AM
+ */
+public class HelloWorld {
+
+    @Test
+    public void canary() {
+        assertThat(true, equalTo(true));
+    }
+}
